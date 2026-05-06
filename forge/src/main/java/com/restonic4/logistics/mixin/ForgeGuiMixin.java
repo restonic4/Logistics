73e436dc301d1a3f3fab0b43a4c6f3e2dbfff0fd
restonic4 <marcosruizlerma@gmail.com>
2026-05-06 13:51:25 +0200

    Forge overlay support

--- forge/src/main/java/com/restonic4/logistics/mixin/ForgeGuiMixin.java
+++ forge/src/main/java/com/restonic4/logistics/mixin/ForgeGuiMixin.java
@@ -1,17 +1,18 @@
-package com.restonic4.logistics.mixin.events;
+package com.restonic4.logistics.mixin;
 
 import com.restonic4.logistics.events.RenderCallbacks;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraftforge.client.gui.overlay.ForgeGui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Gui.class)
-public class GuiMixin {
-    @Inject(method = "render", at = @At(value = "TAIL"), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/PlayerTabOverlay;render(Lnet/minecraft/client/gui/GuiGraphics;ILnet/minecraft/world/scores/Scoreboard;Lnet/minecraft/world/scores/Objective;)V")))
+@Mixin(ForgeGui.class)
+public class ForgeGuiMixin {
+    // I hate Forge so much, why the actual heck would you completely overwrite an entire class. What's this bullshit?
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderColor(FFFF)V"))
     public void logistics$render(GuiGraphics drawContext, float tickDelta, CallbackInfo callbackInfo) {
         RenderCallbacks.ON_HUD_RENDERED.invoker().onEvent(drawContext, tickDelta);
     }
